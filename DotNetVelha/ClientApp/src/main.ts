13dca5755894a0b97b72da1ad4c30bfadad70649
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-28 11:42:18 -0300

    mexendo no arquivo main

--- DotNetVelha/ClientApp/src/main.ts
+++ DotNetVelha/ClientApp/src/main.ts
@@ -1,3 +1,6 @@
+import './polyfills';
+import 'tailwindcss/dist/tailwind.min.css'
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -16,5 +19,12 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic(providers).bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+  // Ensure Angular destroys itself on hot reloads.
+  if (window['ngRef']) {
+    window['ngRef'].destroy();
+  }
+  window['ngRef'] = ref;
+
+  // Otherwise, log the boot error
+}).catch(err => console.error(err));
