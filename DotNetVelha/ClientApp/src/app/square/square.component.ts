c1dd37fb4b4eb04be1f275d1ca1f75c752406330
marcelotakayama <marcelo.takayama00@gmail.com>
2021-01-28 11:56:01 -0300

    ajustando o square component

--- DotNetVelha/ClientApp/src/app/square/square.component.ts
+++ DotNetVelha/ClientApp/src/app/square/square.component.ts
@@ -1,17 +1,56 @@
-import './polyfills';
-import 'tailwindcss/dist/tailwind.min.css'
+import { Component, OnInit, Input } from '@angular/core';
+import { GameService } from "../game.service";
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+@Component({
+  selector: 'app-square',
+  template: `
+    <div 
+      class="game-square rounded-lg border bg-teal-lightest 
+      shadow-md " 
+      (click)="changePlayer()" 
+      [ngClass]="{noClick: gameService.winner} "
+    >
+      <p class="text-grey-darker"> {{ square.state}} </p>
+    </div>
+   `,
+  styles: [`
+    .game-square { 
+      height: 96%; 
+      text-align: center;
+      line-height: 0.85;
+      cursor: pointer;
+    }
 
-import { AppModule } from './app/app.module';
+    p { 
+      display: inline;
+      margin: 0px;
+      font-size: 14rem;
+      overflow: hidden;
+    }
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+    .noClick {
+      pointer-events: none;
+    }`
+  ]
+})
+export class SquareComponent implements OnInit {
+
+  @Input() square; 
+
+  constructor( public gameService: GameService) { }
+
+  ngOnInit() {
   }
-  window['ngRef'] = ref;
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));//4444
+  changePlayer(){ 
+
+    this.gameService.isGameRunning = true;
+
+    if ( this.gameService.isGameRunning && this.square.state === null ){
+      this.square.state =  this.gameService.activePlayer;
+      this.gameService.changePlayerTurn( this.square);
+    }
+    
+  }
+ 
+}
